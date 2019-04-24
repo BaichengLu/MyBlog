ab55dd7c98468ba68f3074541163764748fc4972
Mr.Lu <312808588@qq.com>
2019-04-24 15:57:12 +0800

    模型类的创建

--- typeidea/typeidea/settings/develop.py
+++ typeidea/typeidea/settings/develop.py
@@ -4,7 +4,11 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog_db',
+        'USER': 'blog',
+        'PASSWORD': 'blog123.',
+        'HOST': '192.168.174.130',
+        'PORT': 3306
     }
 }
